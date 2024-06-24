63d90ac77e9ab2d70f7761459e34f4d35007ae70
meet7770 <142961817+meet7770@users.noreply.github.com>
2024-06-24 18:50:47 +0530

    Add files via upload

---
+++ Lab 3/Lab_3_13.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Lab_3_13{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of array : ");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<arr.length;i++){
+			System.out.println("Enter value of "+i+" element : ");
+			arr[i] = sc.nextInt();
+		}
+		for(int i=0;i<arr.length;i++){
+			System.out.println("Value of "+i+" element = "+arr[i]);
+		}
+	}
+}
